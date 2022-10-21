ca83fdf1c866a3f6726cab84e5a023910b552919
Flavio Serrazes <flavio.serrazes@icloud.com>
2022-10-21 13:02:21 +0100

    Create a new iOS project without storyboards

---
+++ Bankey/Bankey/ViewController.swift
@@ -0,0 +1,15 @@
+//  Created on 21.10.22
+//  Copyright © 2022 Flavio Serrazes. All rights reserved.
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
